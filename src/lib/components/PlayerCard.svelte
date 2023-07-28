b17633ee2223ea14c6a518876d5121c24e69a3b1
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-07-28 12:46:19 -0700

    More dummy data and colored the podium

--- src/lib/components/PlayerCard.svelte
+++ src/lib/components/PlayerCard.svelte
@@ -6,7 +6,15 @@
 
 <div class="player-card">
   <div class="left-section">
-    <span>#{ranking}</span>
+    <span
+      class={ranking === 1
+        ? "first"
+        : ranking === 2
+        ? "second"
+        : ranking === 3
+        ? "third"
+        : ""}>#{ranking}</span
+    >
     <span>{name}</span>
   </div>
   <div>{playerStats}/6</div>
@@ -26,5 +34,15 @@
     & .left-section > span {
       padding-inline-end: 0.75rem;
     }
+  }
+
+  .first {
+    color: rgb(255, 222, 33);
+  }
+  .second {
+    color: rgb(193, 190, 190);
+  }
+  .third {
+    color: rgb(183, 123, 58);
   }
 </style>
--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -5,6 +5,9 @@
   let dummyData = [
     { name: "John Doe", ranking: 2, stats: 4.5 },
     { name: "Kyle Mane", ranking: 1, stats: 3.7 },
+    { name: "Tomas Oh", ranking: 4, stats: 5.1 },
+    { name: "Louis", ranking: 6, stats: 5.3 },
+    { name: "Johnathan", ranking: 5, stats: 5.6 },
     { name: "Maria Bonillas", ranking: 3, stats: 5 },
   ];
 
