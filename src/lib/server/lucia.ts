b8b060a2c9d6a65d868d96d07dcc07d48b04516c
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-08-02 13:14:08 -0700

    Updated prisma version

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -1,11 +1,11 @@
 import lucia from "lucia-auth";
-import prisma from "@lucia-auth/adapter-prisma";
+import prismaAdapter from "@lucia-auth/adapter-prisma";
 import { sveltekit } from "lucia-auth/middleware";
 import { dev } from "$app/environment";
 import { prismaClient } from "$lib/server/prisma";
 
 export const auth = lucia({
-  adapter: prisma(prismaClient),
+  adapter: prismaAdapter(prismaClient),
   env: dev ? "DEV" : "PROD",
   middleware: sveltekit(),
   transformDatabaseUser: (userData) => {
